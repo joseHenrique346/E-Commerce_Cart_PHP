c1aa17bc0bb3d5ee7c62ff6c208f4f4231e3ecbb
José Henrique Fernandes <zehenrique0306@gmail.com>
2025-09-08 01:39:48 -0300

    feat(Cart): Criação carrinho - Não finalizado

--- src/Cart.php
+++ src/Cart.php
@@ -1,3 +1,23 @@
 <?php
 
+class Cart
+{
+
+    private array items = [];
+
+    public function addProductToCart(int $productId, int $quantity) : string 
+    {
+        $this->items
+    }
+
+    public function validateExistingProduct() : bool 
+    {
+
+    }
+
+    public function validateProductStockQuantity() : bool
+    {
+        
+    }
+}
 ?>
--- src/Product.php
+++ src/Product.php
@@ -22,10 +22,13 @@ class Product
     #region Getters Setters
     public function getId() : int { return $this->id; }
     public function setId(int $id) : void { $this->id = $id; }
+
     public function getName() : string { return $this->name; }
     public function setName(string $name) : void { $this->name = $name; }
+
     public function getPrice() : float { return $this->price; }
     public function setPrice(float $price) : void { $this->price = $price; }
+    
     public function getStock() : int { return $this->stock; }
     public function setStock(int $stock) : void { $this->stock = $stock; }
     #endregion
